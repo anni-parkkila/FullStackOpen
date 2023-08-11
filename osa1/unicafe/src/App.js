95b8e9d908cc8963db2ca751c45e2a6e4de3051d
Anni Parkkila <anni.parkkila@kaikuhealth.com>
2023-08-11 14:57:37 +0300

    Unicafe step2

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -5,6 +5,7 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const all = good + neutral + bad;
 
   return (
     <div>
@@ -36,9 +37,19 @@ const App = () => {
 
       <h2>Statistics</h2>
 
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
+      <p>
+        Good {good}
+        <br></br>
+        Neutral {neutral}
+        <br></br>
+        Bad {bad}
+        <br></br>
+        All {all}
+        <br></br>
+        Average {(good - bad) / all}
+        <br></br>
+        Positive {(good / all) * 100}%
+      </p>
     </div>
   );
 };
