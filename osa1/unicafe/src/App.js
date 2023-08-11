19c64288b561ac3485a24b2a3d5af921c87ff26c
Anni Parkkila <anni.parkkila@kaikuhealth.com>
2023-08-11 16:44:05 +0300

    Unicafe step5

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,21 +1,26 @@
 import { useState } from "react";
 
+const Button = (props) => (
+  <button onClick={props.buttonClick}>{props.text}</button>
+);
+
+const StatisticLine = (props) => (
+  <div>
+    {props.text} {props.value}
+  </div>
+);
+
 const Statistics = (props) => {
   const all = props.good + props.neutral + props.bad;
   if (all > 0) {
     return (
       <div>
-        Good {props.good}
-        <br></br>
-        Neutral {props.neutral}
-        <br></br>
-        Bad {props.bad}
-        <br></br>
-        All {all}
-        <br></br>
-        Average {(props.good - props.bad) / all}
-        <br></br>
-        Positive {(props.good / all) * 100}%
+        <StatisticLine text="Good" value={props.good} />
+        <StatisticLine text="Neutral" value={props.neutral} />
+        <StatisticLine text="Bad" value={props.bad} />
+        <StatisticLine text="All" value={all} />
+        <StatisticLine text="Average" value={(props.good - props.bad) / all} />
+        <StatisticLine text="Positive" value={(props.good / all) * 100 + "%"} />
       </div>
     );
   } else {
@@ -32,30 +37,10 @@ const App = () => {
   return (
     <div>
       <h1>Give feedback</h1>
-      <button
-        onClick={() => {
-          console.log("good");
-          setGood(good + 1);
-        }}
-      >
-        good
-      </button>
-      <button
-        onClick={() => {
-          console.log("neutral");
-          setNeutral(neutral + 1);
-        }}
-      >
-        neutral
-      </button>
-      <button
-        onClick={() => {
-          console.log("bad");
-          setBad(bad + 1);
-        }}
-      >
-        bad
-      </button>
+      <Button buttonClick={() => setGood(good + 1)} text="good" />
+      <Button buttonClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button buttonClick={() => setBad(bad + 1)} text="bad" />
+
       <h2>Statistics</h2>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
