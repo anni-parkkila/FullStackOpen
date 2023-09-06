0112c59b7f7a5fffc2edb5fea7950cf2d6e5bf40
Anni Parkkila <anni.parkkila@kaikuhealth.com>
2023-09-06 19:44:13 +0300

    Erillinen Course-moduuli

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,48 +1,4 @@
-const Header = ({ name }) => {
-  return <h1>{name}</h1>;
-};
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part) => (
-        <Part key={part.id} part={part} />
-      ))}
-    </div>
-  );
-};
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  );
-};
-
-const Total = ({ parts }) => {
-  const totalExercises = parts.reduce((sum, part) => {
-    return sum + part.exercises;
-  }, 0);
-  return (
-    <div>
-      <p>
-        <strong>Total of {totalExercises} exercises</strong>
-      </p>
-    </div>
-  );
-};
-
-const Course = ({ course }) => {
-  const { name, parts } = course;
-  return (
-    <div>
-      <Header name={name} />
-      <Content parts={parts} />
-      <Total parts={parts} />
-    </div>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
