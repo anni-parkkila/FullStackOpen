6e2b9215bbb1f4ca46899da90cf8ed7d3b3fdd8d
Anni Parkkila <anni.parkkila@kaikuhealth.com>
2024-07-29 19:24:00 +0300

    Step3 API-versio

--- osa2/maiden-tiedot/src/components/Countries.jsx
+++ osa2/maiden-tiedot/src/components/Countries.jsx
@@ -15,64 +15,19 @@ const CountryInfo = ({ country }) => {
   const name = country.name.common;
   const { capital, area, languages, flags, capitalInfo } = country;
   const allLanguages = Object.values(languages);
-  // console.log('capital', capital)
-  // console.log('area', area)
-  // console.log('languages', Object.values(languages))
-  // console.log('flags', flags.png)
-  // console.log('latlng', latlng)
   const latlng = capitalInfo.latlng;
-  console.log('weather state', weather)
 
-  const exampleWeather =
-    {
-        "lat":33.44,
-        "lon":-94.04,
-        "timezone":"America/Chicago",
-        "timezone_offset":-18000,
-        "current":{
-           "dt":1684929490,
-           "sunrise":1684926645,
-           "sunset":1684977332,
-           "temp":292.55,
-           "feels_like":292.87,
-           "pressure":1014,
-           "humidity":89,
-           "dew_point":290.69,
-           "uvi":0.16,
-           "clouds":53,
-           "visibility":10000,
-           "wind_speed":3.13,
-           "wind_deg":93,
-           "wind_gust":6.71,
-           "weather":[
-              {
-                 "id":803,
-                 "main":"Clouds",
-                 "description":"broken clouds",
-                 "icon":"04d"
-              }
-           ]
-        }
+  useEffect(() => {
+    if (country) {
+      weatherService
+        .getWeather(latlng[0], latlng[1])
+        .then(response => {
+          console.log("weather data", response)
+          setWeather(response)})
+        .catch(error => {
+          console.log("error, could not fetch weather data", error)})
     }
-  console.log('temperature', exampleWeather.current.temp)
-
-  //setWeather(exampleWeather);
-
-  // useEffect(() => {
-  //   if (country) {
-  //     weatherService
-  //       .getWeather(latlng[0], latlng[1])
-  //       .then(response => {
-  //         console.log("weather data", response)
-  //         setWeather(response)})
-  //       .catch(error => {
-  //         console.log("error, could not fetch weather data", error)})
-  //       }
-  // }, [country]);
-  console.log('weather', weather);
-  const weatherIcon = `https://openweathermap.org/img/wn/${exampleWeather.current.weather[0].icon}@2x.png`
-  console.log(weatherIcon)
-  const weatherIconAlt = exampleWeather.current.weather[0].description
+  }, [country]);
 
   return (
     <div>
@@ -95,14 +50,15 @@ const CountryInfo = ({ country }) => {
       <div className="flag">
         <img src={flags.svg} alt={flags.alt}/>
       </div>
+      {weather !== null && (
       <div className="weather-info">
           <h3>Weather in {capital}</h3>
           <ul>
-            <li><strong>Temperature:</strong> {exampleWeather.current.temp}°C</li>
-            <li><img src={weatherIcon} alt={weatherIconAlt}/></li>
-            <li><strong>Wind:</strong> {exampleWeather.current.wind_speed} m/s</li>
+            <li><strong>Temperature:</strong> {weather.current.temp}°C</li>
+            <li><img src={`https://openweathermap.org/img/wn/${weather.current.weather[0].icon}@2x.png`} alt={weather.current.weather[0].description}/></li>
+            <li><strong>Wind:</strong> {weather.current.wind_speed} m/s</li>
           </ul>
-      </div>
+      </div>)}
     </div>
   )
 }
--- osa2/maiden-tiedot/src/services/weather.jsx
+++ osa2/maiden-tiedot/src/services/weather.jsx
@@ -4,8 +4,8 @@ const api_key = import.meta.env.VITE_WEATHER_KEY
 const baseUrl = 'https://api.openweathermap.org/data/3.0/onecall?'
 const endUrl = `&units=metric&exclude=minutely,hourly,daily&appid=${api_key}`
 
-const getWeather= (lat, lon) => {
-  console.log('url', `${baseUrl}lat=${lat}&lon=${lon}${endUrl}`)
+const getWeather = (lat, lon) => {
+  //console.log('url', `${baseUrl}lat=${lat}&lon=${lon}${endUrl}`)
   const request = axios.get(`${baseUrl}lat=${lat}&lon=${lon}${endUrl}`)
   return request.then(response => response.data)
 }
