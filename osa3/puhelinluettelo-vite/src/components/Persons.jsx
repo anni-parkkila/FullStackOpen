448bae350b81070a69f34fe0dfd646534f74816a
Anni Parkkila <anni.parkkila@kaikuhealth.com>
2024-07-29 14:15:17 +0300

    Puhelinluettelon Vite-versio

---
+++ osa3/puhelinluettelo-vite/src/components/Persons.jsx
@@ -0,0 +1,30 @@
+const Person = ({ person, removePerson }) => {
+  //console.log("person", person);
+  const { name, phonenumber } = person;
+  return (
+    <div>
+      {name} {phonenumber} <button onClick={removePerson}>delete</button>
+    </div>
+  );
+};
+
+const Persons = ({ filter, persons, removePerson }) => {
+  //   console.log("Persons", persons);
+  //   console.log("filter", filter);
+  const filteredPersons = filter
+    ? persons.filter((person) =>
+        person.name.toLowerCase().includes(filter.toLowerCase())
+      )
+    : persons;
+
+  return (
+    <div>
+      {filteredPersons.map((person) => (
+        <Person key={person.id} person={person} removePerson={() => removePerson(person.id)}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Persons;
