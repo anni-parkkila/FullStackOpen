9f9a638e9ebde76317ac3bcad41f99a5f7809e9a
Anni Parkkila <anni.parkkila@kaikuhealth.com>
2024-09-10 20:00:46 +0300

    Step1 notifikaatio

--- osa7/blogilista/blogilista-frontend/src/App.jsx
+++ osa7/blogilista/blogilista-frontend/src/App.jsx
@@ -1,174 +1,175 @@
 import { useState, useEffect, useRef } from 'react'
+import { useDispatch } from 'react-redux'
 import Blog from './components/Blog'
 import BlogForm from './components/BlogForm'
 import LoginForm from './components/LoginForm'
 import Togglable from './components/Togglable'
 import Notification from './components/Notification'
+import { newNotification } from './reducers/notificationReducer'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import './index.css'
 
 const App = () => {
-    const [blogs, setBlogs] = useState([])
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [user, setUser] = useState(null)
-    const [notification, setNotification] = useState(null)
-    const blogFormRef = useRef()
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const blogFormRef = useRef()
 
-    useEffect(() => {
-        blogService.getAll().then((blogs) => setBlogs(blogs))
-    }, [])
+  const dispatch = useDispatch()
 
-    useEffect(() => {
-        const loggedUserJSON = window.localStorage.getItem('loggedPlokiappUser')
-        if (loggedUserJSON) {
-            const user = JSON.parse(loggedUserJSON)
-            setUser(user)
-            blogService.setToken(user.token)
-        }
-    }, [])
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs))
+  }, [])
 
-    const handleLogin = async (event) => {
-        event.preventDefault()
-        try {
-            const user = await loginService.login({
-                username,
-                password,
-            })
-            window.localStorage.setItem(
-                'loggedPlokiappUser',
-                JSON.stringify(user)
-            )
-            setNotification('Login successful')
-            blogService.setToken(user.token)
-            setUser(user)
-            setUsername('')
-            setPassword('')
-        } catch (exception) {
-            setNotification('ERROR: Wrong username or password')
-        }
-        setTimeout(() => {
-            setNotification(null)
-        }, 5000)
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedPlokiappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
     }
+  }, [])
 
-    const addBlog = (blogObject) => {
-        blogFormRef.current.toggleVisibility()
-        blogService
-            .create(blogObject)
-            .then((returnedBlog) => {
-                setNotification(
-                    `"${returnedBlog.title}" by ${returnedBlog.author} was added to list`
-                )
-                setBlogs(blogs.concat(returnedBlog))
-            })
-            .catch((error) => {
-                setNotification(
-                    'ERROR: all fields must be filled in and be min. 3 characters long'
-                )
-            })
-        setTimeout(() => {
-            setNotification(null)
-        }, 5000)
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      })
+      window.localStorage.setItem('loggedPlokiappUser', JSON.stringify(user))
+      dispatch(newNotification('Login successful', 5))
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      dispatch(newNotification('ERROR: Wrong username or password', 5))
     }
+  }
 
-    const addLike = (blogObject) => {
-        blogService.update(blogObject.id, blogObject).then((returnedBlog) => {
-            setBlogs(
-                blogs.map((blog) =>
-                    blog.id !== blogObject.id ? blog : returnedBlog
-                )
+  const addBlog = (blogObject) => {
+    blogFormRef.current.toggleVisibility()
+    blogService
+      .create(blogObject)
+      .then((returnedBlog) => {
+        dispatch(
+          newNotification(
+            `"${returnedBlog.title}" by ${returnedBlog.author} was added to list`,
+            5
+          )
+        )
+        setBlogs(blogs.concat(returnedBlog))
+      })
+      .catch((error) => {
+        dispatch(
+          newNotification(
+            'ERROR: all fields must be filled in and be min. 3 characters long',
+            5
+          )
+        )
+      })
+  }
+
+  const addLike = (blogObject) => {
+    blogService.update(blogObject.id, blogObject).then((returnedBlog) => {
+      setBlogs(
+        blogs.map((blog) => (blog.id !== blogObject.id ? blog : returnedBlog))
+      )
+    })
+  }
+
+  const removeBlog = (id) => {
+    const blogToRemove = blogs.find((blog) => blog.id === id)
+
+    if (
+      window.confirm(
+        `Remove blog "${blogToRemove.title}" by ${blogToRemove.author}?`,
+        5
+      )
+    ) {
+      blogService
+        .remove(blogToRemove.id)
+        .then(() => {
+          setBlogs(blogs.filter((blog) => blog.id !== id))
+          dispatch(
+            newNotification(`Blog "${blogToRemove.title}" was removed`, 5)
+          )
+        })
+        .catch((error) => {
+          console.log('removing failed:', error)
+          if (error.response.status === 401) {
+            dispatch(
+              newNotification(
+                'ERROR: blogs may only be removed by the user who added them',
+                5
+              )
             )
+          } else {
+            dispatch(newNotification(`ERROR: ${error.message}`, 5))
+          }
         })
     }
+  }
 
-    const removeBlog = (id) => {
-        const blogToRemove = blogs.find((blog) => blog.id === id)
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value)
+  }
 
-        if (
-            window.confirm(
-                `Remove blog "${blogToRemove.title}" by ${blogToRemove.author}?`
-            )
-        ) {
-            blogService
-                .remove(blogToRemove.id)
-                .then(() => {
-                    setBlogs(blogs.filter((blog) => blog.id !== id))
-                    setNotification(`Blog "${blogToRemove.title}" was removed`)
-                })
-                .catch((error) => {
-                    console.log('removing failed:', error)
-                    if (error.response.status === 401) {
-                        setNotification(
-                            'ERROR: blogs may only be removed by the user who added them'
-                        )
-                    } else {
-                        setNotification(`ERROR: ${error.message}`)
-                    }
-                })
-        }
-        setTimeout(() => {
-            setNotification(null)
-        }, 5000)
-    }
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value)
+  }
 
-    const handleUsernameChange = (event) => {
-        setUsername(event.target.value)
-    }
+  const logoutButton = () => {
+    window.localStorage.removeItem('loggedPlokiappUser')
+    setUser(null)
+  }
 
-    const handlePasswordChange = (event) => {
-        setPassword(event.target.value)
-    }
-
-    const logoutButton = () => {
-        window.localStorage.removeItem('loggedPlokiappUser')
-        setUser(null)
-    }
-
-    if (user === null) {
-        return (
-            <div>
-                <h1>Bloglist</h1>
-                <h2>Log in to application</h2>
-                <Notification message={notification} />
-                <LoginForm
-                    username={username}
-                    handleUsernameChange={handleUsernameChange}
-                    password={password}
-                    handlePasswordChange={handlePasswordChange}
-                    handleLogin={handleLogin}
-                />
-            </div>
-        )
-    }
-
+  if (user === null) {
     return (
-        <div>
-            <h2>Blogs</h2>
-            <Notification message={notification} />
-            <div className="user">
-                {user.name} logged in
-                <button onClick={logoutButton}>logout</button>
-            </div>
-            <Togglable buttonLabel="Add a new blog" ref={blogFormRef}>
-                <BlogForm createBlog={addBlog} />
-            </Togglable>
-            <div className="bloglist">
-                {blogs
-                    .sort((a, b) => b.likes - a.likes)
-                    .map((blog) => (
-                        <Blog
-                            key={blog.id}
-                            blog={blog}
-                            updateLikes={addLike}
-                            removeBlog={() => removeBlog(blog.id)}
-                            user={user}
-                        />
-                    ))}
-            </div>
-        </div>
+      <div>
+        <h1>Bloglist</h1>
+        <h2>Log in to application</h2>
+        <Notification />
+        <LoginForm
+          username={username}
+          handleUsernameChange={handleUsernameChange}
+          password={password}
+          handlePasswordChange={handlePasswordChange}
+          handleLogin={handleLogin}
+        />
+      </div>
     )
+  }
+
+  return (
+    <div>
+      <h2>Blogs</h2>
+      <Notification />
+      <div className="user">
+        {user.name} logged in
+        <button onClick={logoutButton}>logout</button>
+      </div>
+      <Togglable buttonLabel="Add a new blog" ref={blogFormRef}>
+        <BlogForm createBlog={addBlog} />
+      </Togglable>
+      <div className="bloglist">
+        {blogs
+          .sort((a, b) => b.likes - a.likes)
+          .map((blog) => (
+            <Blog
+              key={blog.id}
+              blog={blog}
+              updateLikes={addLike}
+              removeBlog={() => removeBlog(blog.id)}
+              user={user}
+            />
+          ))}
+      </div>
+    </div>
+  )
 }
 
 export default App
