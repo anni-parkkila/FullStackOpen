907ce9660427f04d95f54b1546fe6ecacd27a66f
Anni Parkkila <anni.parkkila@kaikuhealth.com>
2024-09-21 18:56:40 +0300

    genrefiksi

--- osa8/kirjasto-frontend/src/components/Books.jsx
+++ osa8/kirjasto-frontend/src/components/Books.jsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useQuery } from '@apollo/client'
 import { ALL_BOOKS } from '../queries'
 
 const Books = ({ books }) => {
+  const [genres, setGenres] = useState([])
   const [genre, setGenre] = useState('')
   let booksToShow = []
+
+  useEffect(() => {
+    setGenres([...new Set(books?.flatMap((book) => book.genres))])
+  }, [books])
 
   const result = useQuery(ALL_BOOKS, {
     variables: { genre },
@@ -21,25 +26,12 @@ const Books = ({ books }) => {
     <div>
       <h2>Books</h2>
       <div>
-        <button type="button" onClick={() => setGenre('refactoring')}>
-          refactoring
-        </button>
-        <button type="button" onClick={() => setGenre('agile')}>
-          agile
-        </button>
-        <button type="button" onClick={() => setGenre('patterns')}>
-          patterns
-        </button>
-        <button type="button" onClick={() => setGenre('design')}>
-          design
-        </button>
-        <button type="button" onClick={() => setGenre('crime')}>
-          crime
-        </button>
-        <button type="button" onClick={() => setGenre('classic')}>
-          classic
-        </button>
-        <button type="button" onClick={() => setGenre('')}>
+        {genres.map((g) => (
+          <button key={g} type="button" onClick={() => setGenre(g)}>
+            {g}
+          </button>
+        ))}
+        <button key="all" type="button" onClick={() => setGenre('')}>
           all genres
         </button>
       </div>
