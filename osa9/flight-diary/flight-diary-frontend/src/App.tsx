4c94c66020724a4db6004e02faf1f724677e3e3b
Anni Parkkila <anni.parkkila@kaikuhealth.com>
2024-09-29 01:00:49 +0300

    Step1-2 GET and POST

--- osa9/flight-diary/flight-diary-frontend/src/App.tsx
+++ osa9/flight-diary/flight-diary-frontend/src/App.tsx
@@ -1,35 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import { DiaryEntry } from "./types";
+import { getAllDiaryEntries, createDiaryEntry } from "./diaryService";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [newDate, setNewDate] = useState("");
+  const [newVisibility, setNewVisibility] = useState("");
+  const [newWeather, setNewWeather] = useState("");
+  const [newComment, setNewComment] = useState("");
+  const [diaryEntries, setDiaryEntries] = useState<DiaryEntry[]>([]);
+
+  useEffect(() => {
+    getAllDiaryEntries().then((data) => {
+      console.log("data", data);
+      setDiaryEntries(data);
+    });
+  }, []);
+
+  const diaryEntryCreation = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    console.log("create new");
+    const newDiaryEntry = {
+      date: newDate,
+      visibility: newVisibility,
+      weather: newWeather,
+      comment: newComment,
+    };
+    createDiaryEntry(newDiaryEntry).then((data) => {
+      setDiaryEntries(diaryEntries.concat(data));
+    });
+    setNewDate("");
+    setNewVisibility("");
+    setNewWeather("");
+    setNewComment("");
+  };
 
   return (
-    <>
+    <div>
+      <h1>Flight Diary</h1>
+      <h2>Add new entry</h2>
+      <form onSubmit={diaryEntryCreation}>
+        date:{" "}
+        <input
+          value={newDate}
+          onChange={(event) => setNewDate(event.target.value)}
+        />
+        <br />
+        visibility:{" "}
+        <input
+          value={newVisibility}
+          onChange={(event) => setNewVisibility(event.target.value)}
+        />
+        <br />
+        weather:{" "}
+        <input
+          value={newWeather}
+          onChange={(event) => setNewWeather(event.target.value)}
+        />
+        <br />
+        comment:{" "}
+        <input
+          value={newComment}
+          onChange={(event) => setNewComment(event.target.value)}
+        />
+        <br />
+        <button type="submit">add</button>
+      </form>
+      <h2>Diary Entries</h2>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {diaryEntries.map((entry) => {
+          return (
+            <div key={entry.id}>
+              <h3>{entry.date}</h3>
+              <ul>
+                <li>visibility: {entry.visibility}</li>
+                <li>weather: {entry.weather}</li>
+              </ul>
+              <hr />
+            </div>
+          );
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    </div>
+  );
+};
+export default App;
