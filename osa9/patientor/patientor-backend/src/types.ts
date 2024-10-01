1e1c1cb0608074b4efdf1e70f3de64c80899bc08
Anni Parkkila <anni.parkkila@kaikuhealth.com>
2024-10-01 12:47:08 +0300

    Step3 Entry types

--- osa9/patientor/patientor-backend/src/types.ts
+++ osa9/patientor/patientor-backend/src/types.ts
@@ -1,5 +1,11 @@
 import { z } from "zod";
-import { EntrySchema, NewPatientSchema } from "./utils";
+import { NewPatientSchema } from "./utils";
+
+export enum EntryType {
+  Hospital = "Hospital",
+  Occupational = "OccupationalHealthcare",
+  Health = "HealthCheck",
+}
 
 export interface Diagnosis {
   code: string;
@@ -7,13 +13,57 @@ export interface Diagnosis {
   latin?: string;
 }
 
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
 export enum Gender {
   Female = "female",
   Male = "male",
   Other = "other",
 }
-
-type Entry = z.infer<typeof EntrySchema>;
 
 export interface Patient {
   id: string;
--- osa9/patientor/patientor-backend/src/utils.ts
+++ osa9/patientor/patientor-backend/src/utils.ts
@@ -1,17 +1,12 @@
 import { NewPatient, Gender } from "./types";
 import { z } from "zod";
 
-export const EntrySchema = z.object({
-  id: z.string(),
-});
-
 export const NewPatientSchema = z.object({
   name: z.string(),
   dateOfBirth: z.string().date(),
   ssn: z.string(),
   gender: z.nativeEnum(Gender),
   occupation: z.string(),
-  entries: z.array(EntrySchema).optional(),
 });
 
 export const toNewPatient = (object: unknown): NewPatient => {
